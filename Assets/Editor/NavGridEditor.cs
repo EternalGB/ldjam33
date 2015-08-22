a56e38be55247ecde411d425197f4a97e0c17953
RaphByrne <raph.byrne@gmail.com>
2015-08-22 17:30:38 +0800

    nav grid

---
+++ Assets/Editor/NavGridEditor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(NavGrid))]
+public class NavGridEditor : Editor 
+{
+
+    
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+		NavGrid ng = (NavGrid)target;
+
+		GUILayout.BeginVertical();
+		if(GUILayout.Button("Make Symmetrical")) 
+        {
+			ng.MakeSymmetrical();
+		}
+        if(GUILayout.Button("Link Neighbours"))
+        {
+            ng.FindNeighboursViaRaycast();
+            ng.MakeSymmetrical();
+        }
+        if(GUILayout.Button("Clear Neighbour List"))
+        {
+            ng.ClearNeighbours();
+        }
+		GUILayout.EndVertical();
+	}
+
+
+    
+}
---
+++ Assets/Scripts/NavGrid.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class NavGrid : MonoBehaviour
+{
+
+	public List<NavPoint> points;
+
+    public LayerMask pointMask, blockingMask;
+
+	public void MakeSymmetrical()
+	{
+		foreach(NavPoint np in points) 
+        {
+			foreach(NavPoint neighbour in np.neighbours) 
+            {
+				if(!neighbour.neighbours.Contains(np))
+					neighbour.neighbours.Add(np);
+			}
+		}
+	}
+
+    Vector3[] searchDirections = 
+    {
+        Vector3.right, 
+        -Vector3.right, 
+        Vector3.forward, 
+        -Vector3.forward
+    };
+
+    public void FindNeighboursViaRaycast()
+    {
+        
+        foreach(NavPoint point in points)
+        {
+            foreach(Vector3 dir in searchDirections)
+            {
+                Ray ray = new Ray(point.transform.position, dir);
+                RaycastHit hitInfo;
+                if(Physics.Raycast(ray, out hitInfo))
+                {
+                    NavPoint neighbour = hitInfo.collider.GetComponent<NavPoint>();
+                    if(neighbour)
+                    {
+                        point.neighbours.Add(neighbour);
+                    }
+                }
+            }
+        }
+    }
+
+    public void ClearNeighbours()
+    {
+        foreach (NavPoint point in points)
+        {
+            point.neighbours.Clear();
+        }
+    }
+
+    bool IsLayerInMask(int layer, LayerMask mask)
+    {
+        return (mask.value & 1 << layer) != 0;
+    }
+
+}
+
