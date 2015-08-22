e1879e5dd619db28ead1d90fd8818ade2d34e8bb
RaphByrne <raph.byrne@gmail.com>
2015-08-22 15:37:44 +0800

    setup unity vs player controller prefabs

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public Vector2 sensitivity, xMinMaxAngle, yMinMaxAngle;
+
+    Quaternion origRotation;
+    Vector2 rot;
+
+    // Use this for initialization
+    void Start()
+    {
+        origRotation = transform.localRotation;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rot.x += Input.GetAxis("Mouse X") * sensitivity.x;
+        rot.y += Input.GetAxis("Mouse Y") * sensitivity.y;
+
+        rot.x = ClampAngle(rot.x, xMinMaxAngle);
+        rot.y = ClampAngle(rot.y, yMinMaxAngle);
+
+        transform.localRotation = origRotation * 
+            Quaternion.AngleAxis(rot.x, Vector3.up) * 
+            Quaternion.AngleAxis(rot.y, -Vector3.right);
+    }
+
+    public float ClampAngle(float angle, Vector2 minMax)
+    {
+        return ClampAngle(angle, minMax.x, minMax.y);
+    }
+
+    public float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360)
+            angle += 360;
+        if (angle > 360)
+            angle -= 360;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    CharacterController cc;
+    [SerializeField]
+    private Vector3 move;
+
+    void Awake()
+    {
+        cc = GetComponent<CharacterController>();
+        move = new Vector3();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float lastY = move.y;
+        move.x = Input.GetAxis("Horizontal");
+        move.y = 0;
+        move.z = Input.GetAxis("Vertical");
+
+        move = transform.TransformDirection(move);
+        move.x *= speed;
+        move.z *= speed;
+        move.y = lastY;
+        cc.Move(move * Time.deltaTime);
+    }
+}
