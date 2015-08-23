ea003132adfd0f4009f954fe1d7a960878de09f2
RaphByrne <raph.byrne@gmail.com>
2015-08-23 21:52:38 +0800

    wall textures

---
+++ Assets/Scripts/WallTextureAssigner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallTextureAssigner : MonoBehaviour
+{
+
+    public Texture[] albedos;
+    public Texture[] normals;
+
+    // Use this for initialization
+    void Start()
+    {
+        MeshRenderer[] renderers = GetComponentsInChildren<MeshRenderer>();
+        foreach(var rend in renderers)
+        {
+            rend.material.SetTexture("_MainTex", albedos[Random.Range(0, albedos.Length)]);
+            rend.material.SetTexture("_BumpMap", normals[Random.Range(0, normals.Length)]);
+        }
+    }
+
+
+}
